d93a5a54e821a6965ab84f9eb3a98e4b406c813b
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-02-25 08:08:39 -0500

    Add products, category_names and subcategory_names routes

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from django.conf import settings
 from food_reference_listing.users.api.views import UserViewSet
+from food_reference_listing.database.api.views import ProductViewSet, CategoryNameViewSet, SubcategoryNameViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -8,7 +9,9 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
-
+router.register("products", ProductViewSet, basename='products')
+router.register("category_names", CategoryNameViewSet, basename='category_names')
+router.register("subcategory_names", SubcategoryNameViewSet, basename='subcategory_names')
 
 app_name = "api"
 urlpatterns = router.urls
